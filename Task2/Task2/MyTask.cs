4746f8111fdaedd071b676694c47ccbc12ce5fef
Andrew <a.oleyn1kov@outlook.com>
2023-12-22 15:51:10 +0300

    fix

--- Task2/Task2/MyTask.cs
+++ Task2/Task2/MyTask.cs
@@ -1,51 +1,55 @@
+using Task2;
 
+public class MyTask<TResult> : IMyTask<TResult> {
+	private volatile Exception _taskExecutionException;
+	private Func<TResult> _taskFunc;
+	private readonly object _lock = new object();
 
-	using Task2;
+	public bool HasContinuationTask => ContinuationTask != null;
+	private bool HasFailed => _taskExecutionException != null;
 
-	public class MyTask<TResult> : IMyTask<TResult> {
-		private Exception _taskExecutionException;
-		private Func<TResult> _taskFunc;
+	public bool IsCompleted => _isCompleted;
+	private volatile bool _isCompleted;
 
-		public bool HasContinuationTask => ContinuationTask != null;
-		private bool HasFailed => _taskExecutionException != null;
+	public TResult Result {
+		get {
+			while (!_isCompleted && !HasFailed) { }
 
-		public bool IsCompleted => _isCompleted;
-		private bool _isCompleted;
+			if (HasFailed) {
+				throw new AggregateException(_taskExecutionException);
+			}
 
-		public TResult Result {
-			get {
-				while (!_isCompleted && !HasFailed) { }
-
-				if (HasFailed) {
-					throw new AggregateException(_taskExecutionException);
-				}
-
+			lock (_lock) {
 				return _result;
 			}
 		}
-		
-		private TResult _result;
+	}
+	
+	private TResult _result;
 
-		public IMyTask ContinuationTask => _continuationTask;
-		private IMyTask _continuationTask;
+	public IMyTask ContinuationTask => _continuationTask;
+	private IMyTask _continuationTask;
 
-		public MyTask(Func<TResult> taskFunc) {
-			_taskFunc = taskFunc;
-		}
+	public MyTask(Func<TResult> taskFunc) {
+		_taskFunc = taskFunc;
+	}
 
-		public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continuation) {
-			var nextTask = new MyTask<TNewResult>(() => continuation.Invoke(Result));
-			_continuationTask = nextTask;
-			return nextTask;
-		}
+	public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continuation) {
+		var nextTask = new MyTask<TNewResult>(() => continuation.Invoke(Result));
+		_continuationTask = nextTask;
+		return nextTask;
+	}
 
-		public void Execute() {
-			try {
-				_result = _taskFunc.Invoke();
-				_isCompleted = true;
+	public void Execute() {
+		try {
+			TResult result = _taskFunc.Invoke();
+			lock (_lock) {
+				_result = result;
 			}
-			catch (Exception e) {
-				_taskExecutionException = e;
-			}
+			_isCompleted = true;
+		}
+		catch (Exception e) {
+			_taskExecutionException = e;
 		}
 	}
+}
